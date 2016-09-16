33d29c0ba5db1055ea9309cf8731e6c87a130bbb
Bilbobx182 <c14474048@mydit.ie>
2016-09-16 15:56:49 +0100

    V.0.0.1

--- Main.py
+++ Main.py
@@ -9,14 +9,19 @@ import csv
 
 # TWITTER VARIABLES
 
+
+def int_if_possible(value):
+    try: return int(value)
+    except: return value
+
 consumer_key = 'DDrwYJLIgDvm7WOi0up9wmdNG'
 consumer_secret = '5VDCEj3dnnxyuirn0sHYR0ULyjDdgApAocAX71C53flJwHlmel'
 access_token = '3339394299-U3AB1hJBHTiuImeyhhoDkEBFDjL2bopSVk425yp'
 access_token_secret = 'j5FMNfZ73Tub0zln1eZg8we92TiXgfZUi0fDshklxaZsK'
 
 # Persistent Variables
-characterfrequency = {}
-nerfedfrequency = {}
+herolist = {}
+nerfedherolist = {}
 
 # TWITTER AUTHENTICATION SETUP
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -31,15 +36,18 @@ currdate=("%s" % i)
 # Gets only the relevant date and removes any trailing characters.
 currdate=currdate[:10]
 
-
-# Doing a file + dictionary for seeing how frequently a character is mentioned.
-reader = csv.reader(open('characterlist.csv'))
+# Reading in the information
+reader = csv.reader(open('nerflist.csv', 'r'))
 for row in reader:
-    key = row[0]
-    if key in characterfrequency:
-        pass
-    characterfrequency[key] = row[1:]
-print(characterfrequency)
+   k, v = row
+   herolist[k] = v
+
+# Converting all the numbers that were strings to ints
+for hero in herolist:
+    herolist[hero] = int(herolist[hero])
+
+
+"""
 
 # checking the '#Overwatch' to see what people mention
 for tweet in tweepy.Cursor(api.search, q='#overwatch', since=currdate ).items(100):
@@ -49,3 +57,11 @@ for tweet in tweepy.Cursor(api.search, q='#overwatch', since=currdate ).items(10
             print(hero + "  Is mentioned below: ")
             print(tweet.text)
             print("------------------")
+
+
+print(herolist['Ana'])
+herolist['Ana']=herolist['Ana']+1
+print(herolist['Ana'])
+
+
+"""
