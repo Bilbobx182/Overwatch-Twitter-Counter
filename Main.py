684573306a8b95c41865db49977195f842addbe5
Bilbobx182 <c14474048@mydit.ie>
2016-09-16 16:32:12 +0100

    V.0.0.2

--- Main.py
+++ Main.py
@@ -1,19 +1,15 @@
-import urllib, json
 import sys
 import tweepy
 import os
-import time
+import pprint
 import datetime
-from tweepy import OAuthHandler
 import csv
 
+# ---------------------------------------------
+# --------------VARIABLES----------------------
+# ---------------------------------------------
+
 # TWITTER VARIABLES
-
-
-def int_if_possible(value):
-    try: return int(value)
-    except: return value
-
 consumer_key = 'DDrwYJLIgDvm7WOi0up9wmdNG'
 consumer_secret = '5VDCEj3dnnxyuirn0sHYR0ULyjDdgApAocAX71C53flJwHlmel'
 access_token = '3339394299-U3AB1hJBHTiuImeyhhoDkEBFDjL2bopSVk425yp'
@@ -28,16 +24,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+# ---------------------------------------------
+# --------------Reading in---------------------
+# ---------------------------------------------
 
-# Getting the date in a format that can be used in the TweePy lib
-i = datetime.datetime.now()
-# formats it
-currdate=("%s" % i)
-# Gets only the relevant date and removes any trailing characters.
-currdate=currdate[:10]
-
-# Reading in the information
-reader = csv.reader(open('nerflist.csv', 'r'))
+reader = csv.reader(open('herolist.csv', 'r'))
 for row in reader:
    k, v = row
    herolist[k] = v
@@ -46,18 +37,40 @@ for row in reader:
 for hero in herolist:
     herolist[hero] = int(herolist[hero])
 
+reader = csv.reader(open('nerflist.csv', 'r'))
+for row in reader:
+    k, v = row
+    nerfedherolist[k] = v
+# Converting all the numbers that were strings to ints
+for hero in nerfedherolist:
+    nerfedherolist[hero] = int(nerfedherolist[hero])
 
-"""
+# ---------------------------------------------
+# ---------------Actual Code-------------------
+# ---------------------------------------------
+
+# Getting the date in a format that can be used in the TweePy lib
+i = datetime.datetime.now()
+# formats it
+currdate=("%s" % i)
+# Gets only the relevant date and removes any trailing characters.
+currdate=currdate[:10]
+
 
 # checking the '#Overwatch' to see what people mention
-for tweet in tweepy.Cursor(api.search, q='#overwatch', since=currdate ).items(100):
+for tweet in tweepy.Cursor(api.search, q='#overwatch', since=currdate ).items(300):
     #looping around to see if they mention an overwatch hero in their tweet.
-    for hero in characterfrequency:
+    for hero in herolist:
         if(hero in tweet.text):
             print(hero + "  Is mentioned below: ")
             print(tweet.text)
+            herolist[hero]=herolist[hero]+1
             print("------------------")
 
+print(herolist)
+
+
+"""
 
 print(herolist['Ana'])
 herolist['Ana']=herolist['Ana']+1
